230b30b851517bf76dbc888b4752645b408c5492
nithin-ragu <nithinragu@rocketmail.com>
2025-05-20 00:20:41 +0530

    code push

--- src/test/java/org/test/Practice.java
+++ src/test/java/org/test/Practice.java
@@ -1,5 +1,20 @@
 package org.test;
 
+import static io.restassured.RestAssured.*;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 public class Practice {
 
+	public static void main(String[] args) {
+		RestAssured.baseURI = "https://reqres.in/api/users?page=2";
+		Response response = given().header("x-api-key", "reqres-free-v1").get();
+
+		int statusCode = response.getStatusCode();
+		System.out.println(statusCode);
+
+		String asPrettyString = response.getBody().asPrettyString();
+		System.out.println(asPrettyString);
+	}
 }
